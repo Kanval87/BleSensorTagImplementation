1d62cd850fcd089c5c50fd1688288c31ad848242
Kanval Kalra <kanval87@gmail.com>
2015-12-09 16:19:21 +0530

    Sensor report timings changed working smooth

--- Application/src/main/java/com/example/android/bluetoothlegatt/SensorTagGatt.java
+++ Application/src/main/java/com/example/android/bluetoothlegatt/SensorTagGatt.java
@@ -59,60 +59,48 @@ import static java.util.UUID.fromString;
 
 public class SensorTagGatt {
 
-    // Useful maps.
-    private static HashMap<String, String> attributes = new HashMap<>();
-    private static HashMap<String, String> serverToConfig = new HashMap<>();
-    private static HashMap<String, String> serverToData = new HashMap<>();
-    private static HashMap<String, String> serverToPeriod = new HashMap<>();
-
-
     public final static UUID UUID_DEVINFO_SERV = fromString("0000180a-0000-1000-8000-00805f9b34fb");
     public final static UUID UUID_DEVINFO_FWREV = fromString("00002A26-0000-1000-8000-00805f9b34fb");
-
     public final static UUID UUID_IRT_SERV = fromString("f000aa00-0451-4000-b000-000000000000");
     public final static UUID UUID_IRT_DATA = fromString("f000aa01-0451-4000-b000-000000000000");
     public final static UUID UUID_IRT_CONF = fromString("f000aa02-0451-4000-b000-000000000000"); // 0: disable, 1: enable
     public final static UUID UUID_IRT_PERI = fromString("f000aa03-0451-4000-b000-000000000000"); // Period in tens of milliseconds
-
     public final static UUID UUID_ACC_SERV = fromString("f000aa10-0451-4000-b000-000000000000");
     public final static UUID UUID_ACC_DATA = fromString("f000aa11-0451-4000-b000-000000000000");
     public final static UUID UUID_ACC_CONF = fromString("f000aa12-0451-4000-b000-000000000000"); // 0: disable, 1: enable
     public final static UUID UUID_ACC_PERI = fromString("f000aa13-0451-4000-b000-000000000000"); // Period in tens of milliseconds
-
     public final static UUID UUID_HUM_SERV = fromString("f000aa20-0451-4000-b000-000000000000");
     public final static UUID UUID_HUM_DATA = fromString("f000aa21-0451-4000-b000-000000000000");
     public final static UUID UUID_HUM_CONF = fromString("f000aa22-0451-4000-b000-000000000000"); // 0: disable, 1: enable
     public final static UUID UUID_HUM_PERI = fromString("f000aa23-0451-4000-b000-000000000000"); // Period in tens of milliseconds
-
     public final static UUID UUID_MAG_SERV = fromString("f000aa30-0451-4000-b000-000000000000");
     public final static UUID UUID_MAG_DATA = fromString("f000aa31-0451-4000-b000-000000000000");
     public final static UUID UUID_MAG_CONF = fromString("f000aa32-0451-4000-b000-000000000000"); // 0: disable, 1: enable
     public final static UUID UUID_MAG_PERI = fromString("f000aa33-0451-4000-b000-000000000000"); // Period in tens of milliseconds
-
     public final static UUID UUID_OPT_SERV = fromString("f000aa70-0451-4000-b000-000000000000");
     public final static UUID UUID_OPT_DATA = fromString("f000aa71-0451-4000-b000-000000000000");
     public final static UUID UUID_OPT_CONF = fromString("f000aa72-0451-4000-b000-000000000000"); // 0: disable, 1: enable
     public final static UUID UUID_OPT_PERI = fromString("f000aa73-0451-4000-b000-000000000000"); // Period in tens of milliseconds
-
     public final static UUID UUID_BAR_SERV = fromString("f000aa40-0451-4000-b000-000000000000");
     public final static UUID UUID_BAR_DATA = fromString("f000aa41-0451-4000-b000-000000000000");
     public final static UUID UUID_BAR_CONF = fromString("f000aa42-0451-4000-b000-000000000000"); // 0: disable, 1: enable
     public final static UUID UUID_BAR_CALI = fromString("f000aa43-0451-4000-b000-000000000000"); // Calibration characteristic
     public final static UUID UUID_BAR_PERI = fromString("f000aa44-0451-4000-b000-000000000000"); // Period in tens of milliseconds
-
     public final static UUID UUID_GYR_SERV = fromString("f000aa50-0451-4000-b000-000000000000");
     public final static UUID UUID_GYR_DATA = fromString("f000aa51-0451-4000-b000-000000000000");
     public final static UUID UUID_GYR_CONF = fromString("f000aa52-0451-4000-b000-000000000000"); // 0: disable, bit 0: enable x, bit 1: enable y, bit 2: enable z
     public final static UUID UUID_GYR_PERI = fromString("f000aa53-0451-4000-b000-000000000000"); // Period in tens of milliseconds
-
     public final static UUID UUID_MOV_SERV = fromString("f000aa80-0451-4000-b000-000000000000");
     public final static UUID UUID_MOV_DATA = fromString("f000aa81-0451-4000-b000-000000000000");
     public final static UUID UUID_MOV_CONF = fromString("f000aa82-0451-4000-b000-000000000000"); // 0: disable, bit 0: enable x, bit 1: enable y, bit 2: enable z
     public final static UUID UUID_MOV_PERI = fromString("f000aa83-0451-4000-b000-000000000000"); // Period in tens of milliseconds
-
-
     public final static UUID UUID_KEY_SERV = fromString("0000ffe0-0000-1000-8000-00805f9b34fb");
     public final static UUID UUID_KEY_DATA = fromString("0000ffe1-0000-1000-8000-00805f9b34fb");
+    // Useful maps.
+    private static HashMap<String, String> attributes = new HashMap<>();
+    private static HashMap<String, String> serverToConfig = new HashMap<>();
+    private static HashMap<String, String> serverToData = new HashMap<>();
+    private static HashMap<String, String> serverToPeriod = new HashMap<>();
 
     static {
         // Sample Services.
@@ -220,12 +208,12 @@ public class SensorTagGatt {
      * Gives the optimal period for the input service.
      */
     public static byte optimalPeriod(String serviceUuid) {
-        if (serviceUuid.equals(UUID_IRT_SERV)) return (byte) 1000;
-        else if (serviceUuid.equals(UUID_HUM_SERV)) return (byte) 1000;
-        else if (serviceUuid.equals(UUID_BAR_SERV)) return (byte) 1000;
-        else if (serviceUuid.equals(UUID_OPT_SERV)) return (byte) 1000;
-        else if (serviceUuid.equals(UUID_MOV_SERV)) return (byte) 1000;
-        else return (byte) 1000;
+        if (serviceUuid.equals(UUID_IRT_SERV)) return (byte) 100;
+        else if (serviceUuid.equals(UUID_HUM_SERV)) return (byte) 100;
+        else if (serviceUuid.equals(UUID_BAR_SERV)) return (byte) 100;
+        else if (serviceUuid.equals(UUID_OPT_SERV)) return (byte) 100;
+        else if (serviceUuid.equals(UUID_MOV_SERV)) return (byte) 100;
+        else return (byte) 100;
     }
 
     /**
